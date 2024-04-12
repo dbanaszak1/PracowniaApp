27b8eae965864e9b97ad181a07b51449d89232c2
dbanaszak1 <dominikbanaszak530@gmail.com>
2024-04-12 12:38:29 +0200

    fix:bannerScaling add:carSearch

--- Frontend/src/components/Banner.tsx
+++ Frontend/src/components/Banner.tsx
@@ -4,32 +4,33 @@ const Banner = () => {
   return (
     <div className="my-20 mx-10">
         <div className="text-center text-3xl font-serif mb-12">Why rent with <span className="text-orange-600">RentMe</span>?</div>
-        <div className='lg:w-1/2 h-[400px] flex m-auto items-center justify-center'>
-            <div className="border border-orange-600 w-72 h-96 overflow-hidden rounded-lg skew-x-12 -skew-y-12">
-                <div className="h-72 w-72 border-b-[1px] border-orange-600">
+        <div className='lg:w-1/2 md:h-[400px] flex m-auto items-center justify-center'>
+            {/*3xCards*/}
+            <div className="border border-orange-600 w-36 h-44 md:w-72 md:h-96 overflow-hidden rounded-lg skew-x-12 -skew-y-12">
+                <div className="h-28 w-36 md:h-72 md:w-72 border-b-[1px] border-orange-600 overflow-hidden">
                     <img src="src/images/ferrari.png" alt="icon"/>
                 </div>
-                <div className='px-4'>
-                    <h2 className="text-lg ">Best price guarantee!</h2>
-                    <h3 className='text-gray-500 text-sm'>Find your dream car for a better price</h3>
+                <div className='md:px-4 px-1 text-center'>
+                    <h2 className="text-xs md:text-lg">Best price guarantee!</h2>
+                    <h3 className='text-gray-500 text-[11px] md:text-sm'>Find your dream car for a better price</h3>
                 </div>
             </div>
-            <div className="border border-orange-600 w-72 h-96 overflow-hidden rounded-lg -translate-y-8 bg-white z-10">
-                <div className="h-72 w-72 border-b-[1px] border-orange-600">
+            <div className="border border-orange-600 w-36 h-48 md:w-72 md:h-96 overflow-hidden rounded-lg -translate-y-8 bg-white z-10">
+                <div className="h-28 w-36 md:h-72 md:w-72 border-b-[1px] border-orange-600 overflow-hidden">
                     <img src="src/images/lambo.png" alt="icon"/>
                 </div>
-                <div className='px-4'>
-                    <h2 className="text-lg ">New and unique cars!</h2>
-                    <h3 className='text-gray-500 text-sm'>Find something that will impress everyone!</h3>
+                <div className='md:px-4 px-1 text-center'>
+                    <h2 className="text-xs md:text-lg">New and unique cars!</h2>
+                    <h3 className='text-gray-500 text-[11px] md:text-sm'>Find something that will impress everyone!</h3>
                 </div>
             </div> 
-            <div className="border border-orange-600 w-72 h-96 overflow-hidden rounded-lg -skew-x-12 skew-y-12">
-                <div className="h-72 w-72 border-b-[1px] border-orange-600">
+            <div className="border border-orange-600 w-36 h-44 md:w-72 md:h-96 overflow-hidden rounded-lg -skew-x-12 skew-y-12">
+                <div className="h-28 w-36 md:h-72 md:w-72 border-b-[1px] border-orange-600 overflow-hidden">
                     <img src="src/images/giftcar.png" alt="icon"/>
                 </div>
-                <div className='px-4'>
-                    <h2 className="text-lg ">Many satisfied clients!</h2> 
-                    <h3 className='text-gray-500 text-sm'>Buy our gift card and satisfy some too!</h3>
+                <div className='md:px-4 px-1 text-center'>
+                    <h2 className="text-xs md:text-lg">Many satisfied clients!</h2> 
+                    <h3 className='text-gray-500 text-[11px] md:text-sm'>Buy our gift card and satisfy some too!</h3>
                 </div>
             </div>   
         </div>
--- Frontend/src/components/CarCard.tsx
+++ Frontend/src/components/CarCard.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
+
 interface Car {
     Car_id: number;
     Name: string;
@@ -14,33 +16,57 @@ interface Car {
   }
   
   const CarCard = () => {
+  //states
     const [cars, setCars] = useState<Car[]>([]);
+    const [searchTerm, setSearchTerm] = useState('');
   
+  //fetching data from backend
     useEffect(() => {
       axios.get('http://localhost:3000/api/cars', {withCredentials: true})
         .then((response) => setCars(response.data))
         .catch((error) => console.error('Error fetching cars:', error));
     }, []);
+
+
   return (
     <div>
-    {cars.map((car,index)=>(
-        <div key={index} className="w-full lg:w-4/5 h-[400px] lg:h-[360px] m-auto flex-row lg:flex my-10 rounded-xl overflow-hidden border-[1px] border-orange-600">
-            <div style={{backgroundImage: `url(${car.url})`}} className="lg:w-2/3 lg:h-full h-1/3 bg-cover"></div>
-            <div className="lg:w-1/3 text-center flex flex-wrap justify-center py-4 px-2 border-t-[1px] lg:border-l-[1px] border-orange-600">
-                <div>
-                    <div className="text-2xl text-orange-600 font-semibold lg:py-4">{car.BrandName} {car.Name}</div>
-                    <div><span className="text-sm text-gray-500">Production year: </span>{car.Production_year}</div>
-                    <div><span className="text-sm text-gray-500">Color: </span>{car.Color}</div>
-                    <div><span className="text-sm text-gray-500">Seats: </span>{car.Seats}</div>
-                    <div className="text-xl text-gray-500">Engine:</div>
-                    <div ><span className="text-sm text-gray-500">Type: </span>{car.Fuel}</div>
-                    <div ><span className="text-sm text-gray-500">Power: </span>{car.Power} HP</div>
-                    <div ><span className="text-sm text-gray-500">Transmission: </span>Manual</div>
-                    <a href={`/offer/${car.Car_id}`}><button className='border-orange-600 border-[1px] lg:mt-6 p-2 m-2 text-orange-600 font-semibold hover:bg-orange-600 hover:text-white duration-500 rounded-lg'>See the offer</button></a>
-                </div>
-            </div>
-        </div>
-    ))}
+      <h1 className="text-center text-3xl font-serif mb-8">Check out our wide offer!</h1>  
+      <div className="w-full flex justify-center">
+        <label className='text-xl px-5 flex items-center'>Find your favorite car:</label>
+        <input
+          className='border-[1px] border-orange-600 p-2 rounded-lg w-1/2 lg:w-1/4'
+          type="text"
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />        
+      </div>
+
+
+      {/*Car map function*/}
+      {cars
+      .filter((car) =>
+            car.Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            car.BrandName.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .map((car,index)=>(
+          <div key={index} className="w-full lg:w-4/5 h-[400px] lg:h-[360px] m-auto flex-row lg:flex my-10 rounded-xl overflow-hidden border-[1px] border-orange-600">
+              <div style={{backgroundImage: `url(${car.url})`}} className="lg:w-2/3 lg:h-full h-1/3 bg-cover"></div>
+              <div className="lg:w-1/3 text-center flex flex-wrap justify-center py-4 px-2 border-t-[1px] lg:border-l-[1px] border-orange-600">
+                  <div>
+                      <div className="text-2xl text-orange-600 font-semibold lg:py-4">{car.BrandName} {car.Name}</div>
+                      <div><span className="text-sm text-gray-500">Production year: </span>{car.Production_year}</div>
+                      <div><span className="text-sm text-gray-500">Color: </span>{car.Color}</div>
+                      <div><span className="text-sm text-gray-500">Seats: </span>{car.Seats}</div>
+                      <div className="text-xl text-gray-500">Engine:</div>
+                      <div ><span className="text-sm text-gray-500">Type: </span>{car.Fuel}</div>
+                      <div ><span className="text-sm text-gray-500">Power: </span>{car.Power} HP</div>
+                      <div ><span className="text-sm text-gray-500">Transmission: </span>Manual</div>
+                      <a href={`/offer/${car.Car_id}`}><button className='border-orange-600 border-[1px] lg:mt-6 p-2 m-2 text-orange-600 font-semibold hover:bg-orange-600 hover:text-white duration-500 rounded-lg'>See the offer</button></a>
+                  </div>
+              </div>
+          </div>
+      ))}
     </div>
   );
 };
